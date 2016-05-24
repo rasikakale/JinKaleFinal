632f00fca317b8f6310f6eeecdbc0b435da8fe8c
rasikakale <rasikakale@gmail.com>
2016-05-23 23:10:21 -0700

    Added Position

---
+++ src/Position.java
@@ -0,0 +1,19 @@
+public class Position {
+ 
+ private int x;
+ private int y;
+ 
+ public Position(int X, int Y) {
+  x = X;
+  y = Y;
+ }
+ 
+ public int getX(){
+  return x;
+ }
+ 
+ public int getY(){
+  return y;
+ }
+
+}
