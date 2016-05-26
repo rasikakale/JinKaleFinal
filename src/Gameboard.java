fc421363ef50a13ccc6707963c50d686988d715c
unknown <rkale178@FHS-LAB127-LE13.fhs.win.fuhsd.org>
2016-05-26 09:12:53 -0700

    Added more to DisplayGame

--- src/Gameboard.java
+++ src/Gameboard.java
@@ -2,21 +2,17 @@
 public class Gameboard {
 
 	private int[][] board;
-	final static int BOMB = -1;
-	final static int FLAG = -2;
+	public final int BOMB = -1;
 	private int row;
 	private int column;
-	private boolean[][] med; // checks to see whether numbers will show or not
-	// on grid
-	// private boolean[][] flags;
-	// private boolean[][] bombs;
-	private boolean[][] show;
 
-	public Gameboard(int NumOfRows, int NumberOfColumns, boolean[][] s) {
+	public int[][] show;
+
+	public Gameboard(int NumOfRows, int NumberOfColumns) {
 		row = NumOfRows;
 		column = NumberOfColumns;
 		board = new int[row][column];
-		show = new boolean[row][column];
+		show = new int[row][column];
 		setBomb();
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < column; j++) {
@@ -27,26 +23,34 @@ public class Gameboard {
 	}
 
 	public void isShowing(int r, int c) {
+		isClicked(r, c);
+		if (board[r][c] == 0) {
+			isClicked(r + 1, c);
+			isClicked(r - 1, c);
+			isClicked(r - 1, c - 1);
+			isClicked(r + 1, c - 1);
+			isClicked(r + 1, c + 1);
+			isClicked(r - 1, c + 1);
+			isClicked(r, c + 1);
+			isClicked(r, c - 1);
+		}
+		if (board[r][c] == BOMB) {
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < column; j++) {
+					show[i][j] = 1;
+
+				}
+			}
+
+		}
+
+	}
+
+	private void isClicked(int r, int c) {
+
 		if (InBoard(r, c)) {
 			if (board[r][c] >= 1 && board[r][c] <= 8) {
-				show[r][c] = true;
-			}
-			if (board[r][c] == 0) {
-				isShowing(r + 1, c);
-				isShowing(r - 1, c);
-				isShowing(r - 1, c - 1);
-				isShowing(r + 1, c - 1);
-				isShowing(r + 1, c + 1);
-				isShowing(r - 1, c + 1);
-				isShowing(r, c + 1);
-				isShowing(r, c - 1);
-			}
-			if (board[r][c] == BOMB) {
-				for (int i = 0; i < row; i++) {
-					for (int j = 0; j < column; j++) {
-						show[i][j] = true;
-					}
-				}
+				show[r][c] = 1;
 			}
 		}
 	}
@@ -63,7 +67,7 @@ public class Gameboard {
 
 	public void setBomb() {
 
-		int numOfBombs = (int) (0.2 * row * column);
+		int numOfBombs = (int) (0.1 * row * column);
 		for (int i = 0; i < numOfBombs; i++) {
 			int randomRow = getRandomRow();
 			int randomCol = getRandomCol();
@@ -71,11 +75,6 @@ public class Gameboard {
 		}
 
 	}
-	/*
-	 * public boolean IsBomb(int r, int c) { return bombs[r][c]; }
-	 * 
-	 * public boolean isFlag(int r, int c) { return flags[r][c]; }
-	 */
 
 	public int getRow() {
 		return row;
@@ -102,24 +101,39 @@ public class Gameboard {
 			for (int j = 0; j < board[0].length; j++) {
 				if (board[i][j] != BOMB) {
 					board[i][j] = getAdjBombs(i, j);
+
 				}
 			}
 		}
-	}
-
-
-	public void resetGame() {
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < column; c++) {
-				med[r][c] = false;
-			}
-
-		}
-
 	}
 
 	public boolean InBoard(int r, int c) {
 		return ((r >= 0 && r < row) && (c >= 0 && c < column));
 	}
 
+	public boolean won() {
+		for (int r = 0; r < board.length; r++) {
+			for (int c = 0; c < board[0].length; c++) {
+				if (board[r][c] != BOMB && getAdjBombs(r, c) != 0) {
+					return false;
+
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean lose() {
+		int sum = 0;
+		for (int r = 0; r < board.length; r++) {
+			for (int c = 0; c < board[0].length; c++) {
+				if (board[r][c] != BOMB) {
+					sum++;
+				}
+
+			}
+		}
+		if (sum == (int) (0.1 * row * column)) return true;
+		return false;
+	}
 }
